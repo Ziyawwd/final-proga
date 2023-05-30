b0b9c5c788e359b6358e31a7939bec73463fc663
Ziyawwd <114824886+Ziyawwd@users.noreply.github.com>
2023-05-30 21:17:20 +0400

    Add files via upload

---
+++ final progect/reg.js
@@ -0,0 +1,6 @@
+let input = document.querySelectorAll('input')
+let submit = document.querySelector('#submit')
+JSON.parse(data)
+submit.addEventListener(click,function () {
+    localStorage.setItem(input)
+})
