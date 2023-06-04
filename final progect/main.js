435a702eac07e46bebd3d09f7857170336c3b2f3
Ziyawwd <114824886+Ziyawwd@users.noreply.github.com>
2023-06-04 19:15:45 +0400

    Add files via upload

--- final progect/main.js
+++ final progect/main.js
@@ -1,1 +1,17 @@
-let input = document.querySelectorAll('input')
+function isInputFull(params) {
+    let allInputs = document.querySelectorAll('input')
+   return [...allInputs].every(input => input.value !== '')
+}
+
+$('button').click(function (e) {
+    e.preventDefault()
+    isInputFull()
+})
+$('input').on('input',function () {
+    $('button').prop('disabled', !isInputFull())
+})
+const users = JSON.parse(localStorage.getItem('users'))
+$('button').click(function (e) {
+    e.preventDefault()
+    console.log(users.some(user=>user.name === allInputs[0].value));
+})
